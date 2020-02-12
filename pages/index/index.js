c30b3fd08433ecad924f8800c7529327ddc10fdb
ljw <1472406718@qq.com>
2020-02-12 12:55:39 +0800

    首页轮播图功能完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,22 @@
 //Page Object
 Page({
   data: {
-    
+    // 轮播图数组
+    swiperList: []
   },
-  //options(Object)
+  // 页面开始加载时触发的声明周期函数
   onLoad: function(options) {
-    
-  },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
-
-  },
-  onUnload: function() {
-
-  },
-  onPullDownRefresh: function() {
-
-  },
-  onReachBottom: function() {
-
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
+    // 发送异步请求获取轮播图数据
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      method: 'GET', // 默认请求方式就是GET
+      success: (result) => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    });
+      
   }
 });
   
