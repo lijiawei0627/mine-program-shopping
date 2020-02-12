c9c04f9c09676b0a91edc927fc22d6a6ec9d55c0
ljw <1472406718@qq.com>
2020-02-12 13:24:02 +0800

    分类导航模块完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,48 @@
-//Page Object
+import { request } from '../../request/index'
 Page({
   data: {
     // 轮播图数组
-    swiperList: []
+    swiperList: [],
+    // 分类导航数组
+    catesList: []
   },
   // 页面开始加载时触发的声明周期函数
-  onLoad: function(options) {
+  onLoad: function (options) {
     // 发送异步请求获取轮播图数据
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-      method: 'GET', // 默认请求方式就是GET
-      success: (result) => {
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   method: 'GET', // 默认请求方式就是GET
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   }
+    // });
+    this.getSwiperList();
+    this.getCateList();
+  },
+  // 获取轮播图数据
+  getSwiperList () {
+    // 使用promise来优化数据请求代码
+    request({ url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata' })
+      .then(result => {
         this.setData({
-          swiperList: result.data.message
+          swiperList: result
         })
-      }
-    });
-      
+      }, err => {
+        console.log(err)
+      })
+  },
+  // 获取分类导航数据
+  getCateList () {
+    request({ url: 'https://api.zbztb.cn/api/public/v1/home/catitems' })
+      .then(result => {
+        console.log(result)
+        this.setData({
+          catesList: result
+        })
+      }, err => {
+        console.log(err)
+      })
   }
 });
-  
