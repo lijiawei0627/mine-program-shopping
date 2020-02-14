93e5ad659aff23981a4e44edb7f5de7dd841e27f
ljw <1472406718@qq.com>
2020-02-14 15:43:53 +0800

    全局加载动画完成

--- request/index.js
+++ request/index.js
@@ -1,4 +1,12 @@
+// 同时发送异步代码的次数
+let ajaxTimes=0;
 export const request=(params)=>{
+  ajaxTimes++;
+  // 显示加载中 效果
+  wx.showLoading({
+    title: "加载中",
+    mask: true
+  });
   const baseUrl = 'https://api.zbztb.cn/api/public/v1'
   return new Promise((resolve,reject)=>{
     wx.request({
@@ -9,7 +17,14 @@ export const request=(params)=>{
      },
      fail:(err)=>{
        reject(err);
+     },
+     complete:()=>{
+      ajaxTimes--;
+      if(ajaxTimes===0){
+      //  关闭加载动画
+       wx.hideLoading();
      }
+    }
     });
   })
 }
